7db01002ee27dca6cfc3e19619aabcdd882c5744
Asraf407 <Asraf.uddin4407@gmail.com>
2022-12-10 20:05:36 +0600

    first commit

---
+++ src/Components/SocialIcons/SocailIcons.js
@@ -0,0 +1,30 @@
+import {
+  faFacebookF,
+  faGithub,
+  faLinkedin,
+  faMediumM,
+} from "@fortawesome/free-brands-svg-icons";
+const icons = [
+  {
+    icon: faGithub,
+    link: "https://github.com/Asraf407",
+    className: "github",
+  },
+  {
+    icon: faMediumM,
+    link: "https://medium.com/@asraf.uddin4407",
+    className: "medium",
+  },
+
+  {
+    icon: faLinkedin,
+    link: "https://www.linkedin.com/in/asraf-uddin-b7264a219/",
+    className: "linkein",
+  },
+  {
+    icon: faFacebookF,
+    link: "https://www.facebook.com/Asraf99uddin",
+    className: "facebook",
+  }
+];
+export default icons;
